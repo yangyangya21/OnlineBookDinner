89ec423d2290f74c3b034ced801ed038c931a819
yangyangya21 <823208782@qq.com>
2021-04-24 19:57:01 +0800

    新建了user表，增加了server模块中的dto层

---
+++ server/src/main/java/com/yjq/server/dto/UserDto.java
@@ -0,0 +1,60 @@
+package com.yjq.server.dto;
+
+/**
+ * 用户数据传输对象  作用于service和controller
+ */
+public class UserDto {
+    private String id;
+
+    private String username;
+
+    private String password;
+
+    private String roleId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", password=").append(password);
+        sb.append(", roleId=").append(roleId);
+        sb.append("]");
+        return sb.toString();
+    }
+}
