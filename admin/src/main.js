3c72c3f2c2684ba91f0ce7790a17370e250767f8
yangyangya21 <823208782@qq.com>
2021-04-26 20:09:24 +0800

    后台管理系统素材引入，登录页面展示

---
+++ admin/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './app.vue'
+import router from './router'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+//定义axios全局名称
+Vue.prototype.$ajax = axios;
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
---
+++ admin/src/views/login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="login_box">
+        <div class="login_l_img"><img src="../../public/static/login/images/login-img.png" /></div>
+        <div class="login">
+            <div class="login_logo"><img src="../../public/static/login/images/login_logo.png" /></div>
+            <div class="login_name">
+                <p>后台管理系统</p>
+            </div>
+            <input name="username" id="user_no" type="text" class="required" placeholder="请输入昵称"/>
+            <input id="password_text" type="password" class="required" placeholder="请输入密码"/>
+            <input id="captcha" type="text" class="required" style="width:150px;margin-bottom: 40px;" placeholder="请输入验证码">
+            <img id="captcha-img" title="点击切换验证码" style="cursor:pointer;" src="" width="135px" height="50px" >
+            <input value="登录" style="width:100%;" type="button" onclick="login();" />
+        </div>
+
+        <div class="copyright">版权所有©2020-2021 杨杨吖 技术支持QQ：823208782</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'login',
+        data: function() {
+            return {
+                user: {}
+            }
+        },
+        methods:{
+        }
+    }
+
+</script>
+
+
+<style>
+</style>
